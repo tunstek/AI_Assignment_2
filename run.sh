27e9fff4de76d65d3d63da559b7e5b151d64063c
Keith Tunstead <tunstek@tcd.ie>
2019-04-30 11:24:26 +0100

    fixed sh script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-conda create -n ai_assignment python=2.7
+conda create -y -n ai_assignment python=2.7
 source activate ai_assignment
 conda install -y -c pytorch pytorch==0.3.1
 conda install -y -c menpo ffmpeg
@@ -7,6 +7,6 @@ conda install -y -c conda-forge opencv
 python -m pip install -y gym==0.9.3
 python -m pip install -y gym[atari]
 
-#python DQL/main.py &> DQL/out.log & disown
-#python DCQL/main.py &> DCQL/out.log & disown
-#python A3C/main.py &> A3C/out.log & disown
+python DQL/main.py &> DQL/out.log & disown
+python DCQL/main.py &> DCQL/out.log & disown
+python A3C/main.py &> A3C/out.log & disown
